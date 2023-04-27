4918ef6d52736a3a3e50288cb54197d785acccfa
dev-9820 <dev.bhanushali03@gmail.com>
2023-04-27 23:49:22 +0530

    Firebase Laptop 27/4

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,44 @@
 import axios from 'axios'
+import {signInWithEmailAndPassword} from 'firebase/auth';
 import NextLink from 'next/link'
 import home from './home'
-import { useCallback, useState } from 'react'
-
+import { useCallback, useState } from 'react';
+import {auth} from "../node_modules/firebase/auth"
 
 export default function login () {
 
-const [loginEmail, setLoginEmail] = useState('');
-const [loginPassword, setLoginPassword] = useState('');
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
 
+
+  const logIN = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth,email,password)
+    .then((userCredential)=> {
+      console.log((userCredential));
+    }).catch((error)=> {
+      console.log(error);
+    })
+    
+  }
 
   return (
     <div type="cont" className='login-container'>
       <title>SolveIT</title>
       <h1>LOGIN</h1>
-      <form>
+      <form onSubmit={logIN}>
         <input 
-        type='text'
+        type='email'
         name='email'
+        value={email}
+        onChange={(e)=> setEmail(e.target.value)}
         placeholder='email'/>
+
         <input 
         type='password'
         name='password'
+        value={password}
+        onChange={(e)=> setPassword(e.target.value)}
         placeholder='password'
         />
         <button type="submit">Submit</button> 
